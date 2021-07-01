eb30580c14bc6d0f7e1fa370126b6efa3fa2a94d
Sasha Kaverina <sasha.kaverina@lewagon.org>
2021-07-01 22:01:49 +0900

    added testing

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -2,7 +2,25 @@ require "application_system_test_case"
 
 class QuestionsTest < ApplicationSystemTestCase
   test "visiting /ask renders the form" do
-    visit ask_url
+    visit '/ask'
     assert_selector "p", text: "Ask your coach anything"
+  end
+
+  test "saying Hello yields a grumpy response from the coach" do
+    visit '/ask'
+    fill_in "question", with: "Hello"
+    click_on "Ask"
+
+    assert_text "I don't care, get dressed and go to work!"
+    take_screenshot
+  end
+
+  test 'saying I am going to work yields a praise' do
+  visit '/ask'
+    fill_in "question", with: "I am going to work"
+    click_on "Ask"
+
+    assert_text "Great!"
+    take_screenshot
   end
 end
